f943438ba706293e5f7504b61575975da5cfec7f
Misakixyz <1706388689@qq.com>
2022-10-09 10:50:46 +0800

    hystrix

--- backup/misaki-hystrix/hystrix/src/main/java/com/misaki/hystrix/HystrixApplication.java
+++ backup/misaki-hystrix/hystrix/src/main/java/com/misaki/hystrix/HystrixApplication.java
@@ -2,7 +2,14 @@ package com.misaki.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+
+@EnableTurbine
+@EnableHystrixDashboard
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HystrixApplication {
 
